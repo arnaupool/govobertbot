94fbf7a3a57f0111cc5b6977db4d2de62eaaecf4
sergiorial <serfgiorico@gmail.com>
2019-06-25 22:21:29 +0200

    El bot ahora tiene comandos basicos, mas que nada para poder manyana ensenyar algo a mas de la encuesta.

--- index.js
+++ index.js
@@ -8,11 +8,43 @@ var questions = [
     {
         title: 'Primera pregunta de mi primera consulta',
         buttons: [
-            [{ text: "Hola", callback_data: 'hola' }],
-            [{ text: "Adios", callback_data: 'adios' }],
+            [{ text: "Hola", callback_data: '1' }],
+            [{ text: "Adios", callback_data: '2' }],
         ]
     }
 ]
+
+telegramApi.onText(/\/start/, (msg) => {
+    startPoll(msg);
+    startPoll.bind(this, msg);
+});
+
+telegramApi.onText(/\/restart/, (msg) => {
+    chat = msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
+    telegramApi.sendMessage(chat, "Funcion no implementada");
+});
+
+telegramApi.onText(/\/return/, (msg) => {
+    chat = msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
+    telegramApi.sendMessage(chat, "Funcion no implementada");
+});
+
+telegramApi.onText(/\/help/, (msg) => {
+    showHelp(msg);
+});
+
+telegramApi.on("callback_query", (msg) => {
+    console.log(msg.from.id);
+    var answer = msg.data;
+    switch(answer) {
+        case "1":
+            telegramApi.sendMessage(msg.from.id, "Hola, como estamos");
+            break;
+        case "2":
+            telegramApi.sendMessage(msg.from.id, "Venga hasta luego");
+            break;
+    }
+})
 
 function startPoll(msg) {
     var arr = questions[0];
@@ -27,20 +59,11 @@ function startPoll(msg) {
     telegramApi.sendMessage(chat, text, options);
 };
 
-telegramApi.on("text", (msg) => {
-    startPoll(msg);
-    startPoll.bind(this, msg);
-});
-
-telegramApi.on("callback_query", (msg) => {
-    console.log(msg.from.id);
-    var answer = msg.data;
-    switch(answer) {
-        case "":
-            telegramApi.sendMessage(msg.from.id, "Hola, como estamos");
-            break;
-        case "":
-            telegramApi.sendMessage(msg.from.id, "Venga hasta luego");
-            break;
-    }
-})
+function showHelp(msg) {
+    var text = "Puedes usar los siguientes comandos!\n" +
+                "/start - Empieza la encuesta\n" +
+                "/restart - Reinicia la encuesta\n" +
+                "/return - Vuelve a la pregunta anterior\n";
+    chat = msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
+    telegramApi.sendMessage(chat, text);
+}
