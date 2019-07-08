a8aa308c549d2a4f95a10f687bcb590095a69c5a
sergiorial <serfgiorico@gmail.com>
2019-07-08 21:35:32 +0200

    Esqueleto para la navegacion hecho, falta rellenar todo el resto.

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
+//#region Imports
 var Telegram = require('node-telegram-bot-api');
 var token = "702738997:AAHyBNRXNfzbPvEGheYQ2rLeUz4o48OE7NQ";
 var bot = new Telegram(token, { polling: true });
 var wrapper = require('node-telegram-keyboard-wrapper');
+//#endregion
 
 var userAnswer = [];
 var isKeyboardOpen = false;
+
+//#region Variables navegar
+const niveles = {
+    INICIO         : 'INICIO',
+    CIUDADANIA     : 'CIUDADANIA',
+    RESPONSABILIDAD: 'RESPONSABILIDAD',
+    TECNOLOÍA      : 'TECNOLOGIA',
+    COMUNICACION   : 'COMUNICACION',
+    GOBIERNO       : 'GOBIERNO',
+    ATRAS          : 'Atrás'
+}
+
+var nivelActual = niveles.INICIO;
+
+//#region Keyboards
 const keyboard = new wrapper.ReplyKeyboard();
 keyboard
-    .addRow("Test1",  "Test2")
-    .addRow("Cerrar");
+    .addRow("CIUDADANIA", "RESPONSABILIDAD")
+    .addRow("TECNOLOÍA" , "COMUNICACION")
+    .addRow("GOBIERNO"  , "ENCUESTA");
 
-var questions = [
+var Opciones = [
     {
-        title: 'Primera pregunta de mi primera consulta',
+        title: 'CIUDADANIA',
         buttons: [
-            [{ text: "Hola", callback_data: '1' }],
-            [{ text: "Adios", callback_data: '2' }],
+            [{ text: "Colaboración"               , callback_data: '1'  }],
+            [{ text: "Cocreación"                 , callback_data: '2'  }],
+            [{ text: "Empoderamiento ciudadano"   , callback_data: '3'  }],
+            [{ text: "Presupuestos participativos", callback_data: '4'  }],
+            [{ text: "Participación ciudadana"    , callback_data: '5'  }],
+            [{ text: "Codiseño"                   , callback_data: '6'  }],
+            [{ text: "Derecho de acceso"          , callback_data: '7'  }],
+            [{ text: "Ciudadano inteligente"      , callback_data: '8'  }],
+            [{ text: "Conciencia cívica"          , callback_data: '9'  }],
+            [{ text: "Cultura participativa"      , callback_data: '10' }],
         ]
     }
 ];
+//#endregion
 
+//#region Commands
 bot.onText(/\/start/, (msg) => {
     startPoll(msg);
     startPoll.bind(this, msg);
@@ -44,31 +72,118 @@ bot.onText(/\/test/, (msg) => {
     isKeyboardOpen = true;
     bot.sendMessage(msg.from.id, text = "Test", keyboard.open({ resize_keyboard: true }));
 });
+//#endregion
 
-bot.on("polling_error", (err) => console.log(err));
-
+//#region Navegacion
 bot.on("message", (msg) => {
-    if (msg.text == "Cerrar" && isKeyboardOpen) {
-        bot.sendMessage(msg.from.id, "Cerrando botones", keyboard.close());
-        isKeyboardOpen != isKeyboardOpen;
+    console.log(msg.text);
+    switch (msg.text) {
+        case niveles.CIUDADANIA:
+            nivelActual = niveles.CIUDADANIA
+            startPoll(msg, 0);
+            startPoll.bind(this, msg);
+            break;
+        case niveles.RESPONSABILIDAD:
+            nivelActual = niveles.RESPONSABILIDAD
+            startPoll(msg, 1);
+            startPoll.bind(this, msg);
+            break;
+        case niveles.TECNOLOGIA:
+            nivelActual = niveles.TECNOLOGIA
+            startPoll(msg, 2);
+            startPoll.bind(this, msg);
+            break;
+        case niveles.COMUNICACION:
+            nivelActual = niveles.COMUNICACION
+            startPoll(msg, 3);
+            startPoll.bind(this, msg);
+            break;
+        case niveles.GOBIERNO:
+            nivelActual = niveles.GOBIERNO
+            startPoll(msg, 4);
+            startPoll.bind(this, msg);
+            break;
     }
 });
 
-bot.on("callback_query", (msg) => {
-    console.log(msg.from.id);
+function handleCiudadania(msg) {
     var answer = msg.data;
     switch(answer) {
-        case "1":
-            bot.sendMessage(msg.from.id, "Hola, como estamos");
+        case '1':
+            bot.sendMessage(msg.from.id, '1');
             break;
-        case "2":
-            bot.sendMessage(msg.from.id, "Venga hasta luego");
+        case '2':
+            bot.sendMessage(msg.from.id, '2');
+            break;
+        case '3':
+            bot.sendMessage(msg.from.id, '3');
+            break;
+        case '4':
+            bot.sendMessage(msg.from.id, '4');
+            break;
+        case '5':
+            bot.sendMessage(msg.from.id, '5');
+            break;
+        case '6':
+            bot.sendMessage(msg.from.id, '6');
+            break;
+        case '7':
+            bot.sendMessage(msg.from.id, '7');
+            break;
+        case '8':
+            bot.sendMessage(msg.from.id, '8');
+            break;
+        case '9':
+            bot.sendMessage(msg.from.id, '9');
+            break;
+        case '10':
+            bot.sendMessage(msg.from.id, '10');
+            break;
+    }
+}
+
+function handleResponsabiliadad(msg) {
+    nivelActual = niveles.RESPONSABILIDAD;
+    keyboard.close();
+    keyboard = keyboardResponsabilidad;
+    keyboard.open({ resize_keyboard: true })
+}
+
+function handleTeconologia(msg) {
+    nivelActual = niveles.TECNOLOGIA;
+    keyboard.close();
+    keyboard = keyboardTecnologia;
+    keyboard.open({ resize_keyboard: true })
+}
+
+function handleComunicacion(msg) {
+    nivelActual = niveles.COMUNICACION;
+    keyboard.close();
+    keyboard = keyboardComunicacion;
+    keyboard.open({ resize_keyboard: true })
+}
+
+function handleGobierno(msg) {
+    nivelActual = niveles.GOBIERNO;
+    keyboard.close();
+    keyboard = keyboardGobierno;
+    keyboard.open({ resize_keyboard: true })
+}
+//#endregion
+
+bot.on("polling_error", (err) => console.log(err));
+
+bot.on("callback_query", (msg) => {
+    console.log(msg.from.id);
+    switch(nivelActual) {
+        case niveles.CIUDADANIA:
+            handleCiudadania(msg);
             break;
     }
 })
 
-function startPoll(msg) {
-    var arr = questions[0];
+function startPoll(msg, index) {
+    var arr = Opciones[index];
     var text = arr.title;
     var options = {
         reply_markup: JSON.stringify({
@@ -87,4 +202,4 @@ function showHelp(msg) {
                 "/return - Vuelve a la pregunta anterior\n";
     chat = msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
     bot.sendMessage(chat, text);
-}
+};
