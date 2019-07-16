882647fbf120419298ec9320d3124030cd5bf458
arnaupool <arnyr@hotmail.es>
2019-07-16 23:25:52 +0200

    Comando csv pulido y acabado

--- exportCSV.js
+++ exportCSV.js
@@ -2,13 +2,16 @@ var exec = require('child_process').exec;
 var mongoExport;
 
 module.exports = {
-    export2csv: function () {
+    export2csv: function (callback) {
+        var creation = new Date();
         var child = exec('mongoexport.bat', function (error, stdout, stderr) {
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
             if (error !== null) {
               console.log('exec error: ' + error);
             }
+
+            callback(creation);
         });
     }
 }
