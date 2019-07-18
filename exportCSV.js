1e1801e8ac60f3b46eb1b0b5303b84c37d3d5e02
arnaupool <arnyr@hotmail.es>
2019-07-18 02:55:01 +0200

    Cambios menores

--- exportCSV.js
+++ exportCSV.js
@@ -4,14 +4,26 @@ var mongoExport;
 module.exports = {
     export2csv: function (callback) {
         var creation = new Date();
-        var child = exec('mongoexport.bat', function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error !== null) {
-              console.log('exec error: ' + error);
-            }
-
+        //mongoexport /db botdb /c consultes /type:csv /o Registro.csv /f _id,userid,concepte,terme,dia,mes,any,hora,min
+        var command = 'mongoexport --db botdb --collection consultes --fields _id,userid,concepte,terme,dia,mes,any,hora,min --csv --out Registro.csv'
+        if (process.platform == 'win32') {  
+            var child = exec('mongoexport.bat', function (error, stdout, stderr) {
+                    console.log('stdout: ' + stdout);
+                    console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                        console.log('win exec error: ' + error);
+                    }
             callback(creation);
-        });
+            });
+        } else {
+            var child = exec(command, function(error, stdout, stderr){
+                console.log('stdout: ' + stdout);
+                    console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                        console.log('deb exec error: ' + error);
+                    }
+            callback(creation);
+            });
+        }
     }
 }
